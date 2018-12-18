338939feaae7ed635dc19d56de148182d4c348ec
Hung <hotro.giaitri23@gmail.com>
2018-12-18 23:06:58 +0700

    sua thong ke

--- src/java/controller/chartServlet.java
+++ src/java/controller/chartServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class chartServlet extends HttpServlet {
 
@@ -15,9 +16,9 @@ public class chartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.setAttribute("listItem", chartDAO.getAll());
-        RequestDispatcher rd = getServletContext().getRequestDispatcher("/admin/chart.jsp");
-        rd.forward(request, response);
+        HttpSession session = request.getSession(false);
+        session.setAttribute("listItem", chartDAO.getAll());
+        response.sendRedirect("admin/chart.jsp");
     }
 
     @Override
